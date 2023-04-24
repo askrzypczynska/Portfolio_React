1945c7b128f7631ff17407c96568624760c59193
askrzypczynska <aleksandraskrzypczynska@gmail.com>
2023-04-24 17:02:48 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SelectionWindow from './Components/SelectionWindow/SelectionWindow';
+
+function App() {
+  return (
+    <div className='App'>
+      <SelectionWindow/>
+      {/* <ItSection/>
+      <GraphicSection/> */}
+    </div>
+  );
+}
+
+export default App;
